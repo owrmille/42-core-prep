71d915d0999fbb03fcdbbfec0eea1a19622a853b
owrmille <iasemin.topchu@gmail.com>
2023-10-09 03:05:02 +0200

    Add files isalpha.c and libft.h from project Libft

---
+++ Libft/isalpha.c
@@ -0,0 +1,17 @@
+#include "libft.h"
+
+int	ft_isalpha(char	*str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if ((str[i] >= 'A' && str[i] <= 'Z') 
+			|| (str[i] >= 'a' && str[i] <= 'z'))
+			i++;
+		else
+			return (0);
+	}
+	return (1);
+}
