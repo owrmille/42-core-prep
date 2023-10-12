09771cfaed4867c955f1619213dfe3b6fdddcd1a
owrmille <iasemin.topchu@gmail.com>
2023-10-12 23:58:23 +0200

    Add argv to test system in Libft

--- Libft/test.c
+++ Libft/test.c
@@ -32,65 +32,85 @@ FunctionCallback int_orig_functions[] = {&isalpha,
 								&toupper,
 								&tolower};
 
-int	main()
+
+int	main(int argc, char **argv)
 {
-	// int var = 97;
-	// const int	funcs_num = 7;
-	// char	*funcs_names[funcs_num] = {"isalpha",
-	// 				"isdigit",
-	// 				"isalnum",
-	// 				"isascii",
-	// 				"isprint",
-	// 				"toupper",
-	// 				"tolower"};
-	// int i = 0;
-	// printf("\nVariable: %d or %c\n\n", var, var);
-	// while (i < funcs_num)
-	// {
-	// 	printf("Function: %s, Result: %d", 
-	// 			funcs_names[i], int_ft_functions[i](var));
-	// 	if (strcmp(funcs_names[i], "toupper") == 0 || strcmp(funcs_names[i], "tolower") == 0)
-	// 		printf(" or %c", int_ft_functions[i](var));
-	// 	if (int_ft_functions[i](var) == int_orig_functions[i](var))
-	// 		printf("\nSUCCESS: original and ft results matched!\n\n");
-	// 	else
-	// 		printf("\nFAIL: check your ft function.\n\n");
-	// 	i++;
-	// }
-	
-	// ft_strlen
-	// printf("Length of 'meow': %d\n", ft_strlen("meow"));
-	
-	//ft_memset
-	// this does not work due to (BUS ERROR: 10):
-	//printf("Before: meow\n After: %s\n", ft_memset("meow", 43, 3));
-	// this works! :
-	// char *str;
-	// str = strdup("hello");
-	// ft_memset(str, '!', 3);
-	// printf("%s\n", str);
+	if (argc == 2)
+	{
+		char *function_name = argv[1];
 
-	//ft_bzero
-	// char *str;
-	// str = strdup("hello");
-	// ft_bzero(str, 3);
-	// printf("ft_bzero: %s\n", str);
-	// /////
-	// char *str2;
-	// str2 = strdup("hello");
-	// bzero(str2, 1);
-	// printf("bzero: %s\n", str2);
+		if (!strcmp(function_name, "isalpha")
+				|| !strcmp(function_name, "isdigit")
+				|| !strcmp(function_name, "isalnum")
+				|| !strcmp(function_name, "isascii")
+				|| !strcmp(function_name, "isprint")
+				|| !strcmp(function_name, "toupper")
+				|| !strcmp(function_name, "tolower"))
+		{
+			int var = 97;
+			const int	funcs_num = 7;
+			char	*funcs_names[funcs_num] = {"isalpha",
+							"isdigit",
+							"isalnum",
+							"isascii",
+							"isprint",
+							"toupper",
+							"tolower"};
+			int i = 0;
+			printf("\nVariable: %d or %c\n\n", var, var);
+			while (i < funcs_num)
+			{
+				printf("Function: %s, Result: %d", 
+						funcs_names[i], int_ft_functions[i](var));
+				if (strcmp(funcs_names[i], "toupper") == 0 || strcmp(funcs_names[i], "tolower") == 0)
+					printf(" or %c", int_ft_functions[i](var));
+				if (int_ft_functions[i](var) == int_orig_functions[i](var))
+					printf("\nSUCCESS: original and ft results matched!\n\n");
+				else
+					printf("\nFAIL: check your ft function.\n\n");
+				i++;
+			}
+		}
+		else if (!strcmp(function_name, "strlen"))
+		{
+			printf("Length of 'meow': %d\n", ft_strlen("meow"));
+		}
+		// this does not work due to (BUS ERROR: 10):
+		//printf("Before: meow\n After: %s\n", ft_memset("meow", 43, 3));
+		// this works! :
+		else if (!strcmp(function_name, "memset"))
+		{
+			char *str;
+			str = strdup("hello");
+			ft_memset(str, '!', 3);
+			printf("%s\n", str);
+		}
+		else if (!strcmp(function_name, "bzero"))
+		{
+			char *str;
+			str = strdup("hello");
+			ft_bzero(str, 3);
+			printf("ft_bzero: %s\n", str);
+			/////
+			char *str2;
+			str2 = strdup("hello");
+			bzero(str2, 1);
+			printf("bzero: %s\n", str2);
+		}
+		else if (!strcmp(function_name, "strncmp"))
+		{
+			char s1[] = "ABD";
+			char s2[] = "ABF";
+			printf("ft_strncmp: (%d)\n", ft_strncmp(s1, s2, 3));
+			printf("strncmp: (%d)\n", strncmp(s1, s2, 3));
+		}
+		else if (!strcmp(function_name, "atoi"))
+		{
+		char	*str = " 	-2147483649a";
+		printf("ft_atoi: (%d)\n", ft_atoi(str));
+		printf("atoi: (%d)\n", atoi(str));
+		}
 
-	// ft_strncmp
-	char s1[] = "ABD";
-	char s2[] = "ABF";
-	printf("ft_strncmp: (%d)\n", ft_strncmp(s1, s2, 3));
-	printf("strncmp: (%d)\n", strncmp(s1, s2, 3));
-
-	//ft_atoi
-	// char	*str = " 	-2147483649a";
-	// printf("ft_atoi: (%d)\n", ft_atoi(str));
-	// printf("atoi: (%d)\n", atoi(str));
-
+	}
 	return (0);
 }
