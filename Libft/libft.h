6521e5ea1025196b8e757caf53e0d684bfbe85a6
owrmille <iasemin.topchu@gmail.com>
2023-10-21 22:39:52 +0200

    Add ft_putendl_fd in Libft

--- Libft/libft.h
+++ Libft/libft.h
@@ -2,6 +2,7 @@
 # define LIBFT_H
 
 # include <stdlib.h>
+# include <unistd.h>
 
 int	ft_isalpha(int c);
 int	ft_isdigit(int c);
@@ -33,5 +34,6 @@ char	*ft_strtrim(char const	*s1, char const	*set);
 char	*ft_strmapi(char const	*s, char	(*f)(unsigned int, char));
 void	ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char	*s, int fd);
+void	ft_putendl_fd(char	*s, int fd);
 
 #endif
