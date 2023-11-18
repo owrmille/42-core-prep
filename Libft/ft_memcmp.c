026347e6ad07180ef7a330396f67dfe7e2339ac0
owrmille <iasemin.topchu@gmail.com>
2023-11-18 15:31:32 +0100

    Convert values to unsigned char + add n=0 case in ft_memcmp in Libft

--- Libft/ft_memcmp.c
+++ Libft/ft_memcmp.c
@@ -2,12 +2,18 @@
 
 int	ft_memcmp(const void	*s1, const void	*s2, size_t n)
 	{
-		const char	*c_s1 = (const char *)s1;
-		const char	*c_s2 = (const char *)s2;
-		int	i;
+		char	*c_s1;
+		char	*c_s2;
+		size_t	i;
 
+		c_s1 = (char *)s1;
+		c_s2 = (char *)s2;
 		i = 0;
-		while ((c_s1[i] == c_s2[i]) && ((size_t)i < n - 1))
+		if (n == 0)
+		{
+			return (0);
+		}
+		while ((c_s1[i] == c_s2[i]) && (i < n - 1))
 			i++;
-		return (c_s1[i] - c_s2[i]);
+		return ((unsigned char)c_s1[i] - (unsigned char)c_s2[i]);
 	}
