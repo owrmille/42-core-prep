2bd844a02083cb1dc2b661f95bd78cb8b9964cea
owrmille <iasemin.topchu@gmail.com>
2023-11-26 00:51:02 +0100

    Rewrite ft_strlcpy in Libft

--- Libft/ft_strlcpy.c
+++ Libft/ft_strlcpy.c
@@ -1,30 +1,14 @@
 #include "libft.h"
 
-size_t	ft_strlcpy(char	*dst, const char	*src, size_t dstsize)
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
 {
-	char const	*s;
-	char	*d;
-	char	*end_d;
+	size_t	src_len;
 
-	s = src;
-	d = dst;
-	end_d = dst + dstsize;
-
-	while (*s != '\0' && end_d > d)
-	{
-		*d = *s;
-		d++;
-		s++;
-	}
-	if (end_d > d)
-	{
-		*d = '\0';
-	}
-	else if (dstsize > 0)
-	{
-		d[-1] = '\0';
-	}
-	while (*s != '\0')
-		s++;
-	return (s - src);
+	src_len = ft_strlen(src);
+	if (NULL == dst || NULL == src || !dstsize)
+		return (src_len);
+	while (*src && --dstsize)
+		*dst++ = *src++;
+	*dst = '\0';
+	return (src_len);
 }
